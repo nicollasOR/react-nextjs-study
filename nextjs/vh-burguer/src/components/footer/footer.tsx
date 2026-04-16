9365d8595fac0b39291d1d306af691360a09ce32
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-16 10:43:06 -0300

    � feat: footer finished �

---
+++ nextjs/vh-burguer/src/components/footer/footer.tsx
@@ -0,0 +1,22 @@
+import styles from '@/components/footer/footer.module.css'
+
+const Footer = () => {
+        return(
+            <footer id={styles.footer}>
+                <nav id={styles.parte_sup}>
+                    <img src="../imgs/Logo_footer.svg" alt="" />
+                    <div id={styles.footer_links}>
+                    <a href="" id={styles.nav_tiktok    }><img src="../imgs/tiktok.svg"    className="nav_img" alt="" />   </a>
+                    <a href="" id={styles.nav_facebook  }><img src="../imgs/facebook.svg"  className="nav_img" alt="" />     </a>
+                    <a href="" id={styles.nav_instagram }><img src="../imgs/instagram.svg" className="nav_img" alt="" />    </a>
+                    <a href="" id={styles.nav_youtube   }><img src="../imgs/yt.svg"        className="nav_img" alt="" />  </a>
+                    </div>
+                </nav>
+                <hr id={styles.linha} />
+                <p id={styles.texto_footer}>Copyright © 2025 VH Burguer | Todos os direitos reservados</p>
+
+            </footer>
+        )
+}
+
+export default Footer;
--- nextjs/vh-burguer/src/pages/home/index.tsx
+++ nextjs/vh-burguer/src/pages/home/index.tsx
@@ -1,9 +1,20 @@
 import Header from "../../components/header/header"
-
+import Footer from "../../components/footer/footer"
 const Home = () => {
     return (
         <>
         <Header/>
+        <main>
+            <section>
+                <h1></h1>
+                <img src="" alt="" />
+                <div>
+                <button></button>
+                <button></button>
+                </div>
+            </section>
+        </main>
+        <Footer/>
         </>
     )
 }
