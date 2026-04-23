b3be8f9fd3ecc21b54fb7cbc694e0e8c5cb690b0
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-23 10:08:51 -0300

    � feat: atualizando listaProduto �

--- nextjs/vh-burguer/src/components/lista-produto/Lista_produto.tsx
+++ nextjs/vh-burguer/src/components/lista-produto/Lista_produto.tsx
@@ -3,25 +3,27 @@ import styles from './listaProduto.module.css'
 
 const ListaProduto = () => {
     return (
-        <>
-        
-                
-                <div id={styles.botoes}>
-                <button> Filtrar <img src="../imgs/edicoesF.svg" id={styles.editar}  alt="" /></button>
+        <section id={`${styles.cardapio} layout_guide`}>
+            <h2 id={styles.h2}>CARDÁPIO</h2>
+            {/* componente da lista cardápio */}
+
+
+
+            <div id={styles.botoes}>
+                <button> Filtrar <img src="../imgs/edicoesF.svg" id={styles.editar} alt="" /></button>
                 <div id={styles.botoes_adm}>
-                <button> Todas as promoções              </button>
-                <button> Adicionar produto               </button>
+                    <button> Todas as promoções              </button>
+                    <button> Adicionar produto               </button>
                 </div>
-                </div>
+            </div>
 
             <ul id={styles.hamburgueres_lado_sup}>
-                <CardProduto/>
-                <CardProduto/>
-                <CardProduto/>
+                <CardProduto />
+                <CardProduto />
+                <CardProduto />
             </ul>
 
-
-        </>
+        </section>
     )
 }
 
