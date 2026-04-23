938e981fe11ed22f40cb95850696fdd3712d94a8
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-23 10:09:27 -0300

    � feat: atualizando sub_header �

--- nextjs/vh-burguer/src/components/sub-header/sub_header.tsx
+++ nextjs/vh-burguer/src/components/sub-header/sub_header.tsx
@@ -1,11 +1,11 @@
+import Link from 'next/link'
 import styles from '../sub-header/sub_header.module.css'
-
 const sub_header = () => {
     return(
         <>
         <header id={styles.header}>
             <img src="../imgs/Logo_footer.svg" id={styles.img_footer} alt="" />
-            <a href="" id={styles.links}>Voltar</a>
+            <Link href="/home" id={styles.links}>Voltar</Link>
         </header>
         
         </>
