a4a206b47d869179a3ef27b9315ef0f728e42893
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-24 11:03:15 -0300

    � feat: add produto �

--- nextjs/vh-burguer/src/pages/produto/index.tsx
+++ nextjs/vh-burguer/src/pages/produto/index.tsx
@@ -15,25 +15,25 @@ const criarProduto = () => {
                     <input type="text" name="nome" placeholder='BBQ Especial' />
             </div>
 
-            <div className={styles.inserir_dados}>
+            <div className={styles.inserir_dados} id={styles.descricao}>
                 <label htmlFor="email">Descrição</label>
                 <input type="text" 
                 placeholder='Hamburguer com molho barbecue defumado com cebola caramelizada.' />
             </div>
 
             <div className={styles.inserir_dados}>
-                <label htmlFor="preco">Preço (R$)</label>
+                <label htmlFor="preco">Preço(R$)</label>
                         <input type="text" name="preco" placeholder='40,00' />
             </div>
 
-            <div className={styles.inserir_dados}>
+            <div className={styles.inserir_dados} id={styles.select2}>
                 <label htmlFor="categorias">Categoria</label>
-                    <select name="" id="">
+                    <select name="" id={styles.select}>
                         <option value=""></option>
                         <option value=""></option>
-                        <option value=""></option>
+                        <option value="" id={styles.option}></option>
                     </select>
-                <Link href="/login"> Adicionar categoria</Link>
+                <Link href="/login" id={styles.adicionarC}> Adicionar categoria</Link>
             </div>
 
             <div className={styles.inserir_dados}>
@@ -42,8 +42,7 @@ const criarProduto = () => {
             </div>
 
             <div className={styles.enviar_botoes}>
-            <button>Adicionar Promoção</button>
-            <button>Salvar</button>
+            <button id={styles.add_save}>Salvar</button>
             </div>
             </form>       
         </main>
