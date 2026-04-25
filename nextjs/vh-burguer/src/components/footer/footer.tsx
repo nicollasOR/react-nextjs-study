697feeae0bda274c6e2c646428de105ca1434411
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-25 00:42:11 -0300

    ♻️  feat: refactoring footer ♻️

--- nextjs/vh-burguer/src/components/footer/footer.tsx
+++ nextjs/vh-burguer/src/components/footer/footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
             <footer id={styles.footer}>
                 <nav id={styles.parte_sup}>
                     <img src="../imgs/Logo_footer.svg" alt="" />
-                    <div id={styles.footer_links}>
+                <div id={styles.footer_links}>
                     <a href="" id={styles.nav_tiktok    }><img src="../imgs/tiktok.svg"    className="nav_img" alt="" />   </a>
                     <a href="" id={styles.nav_facebook  }><img src="../imgs/facebook.svg"  className="nav_img" alt="" />     </a>
                     <a href="" id={styles.nav_instagram }><img src="../imgs/instagram.svg" className="nav_img" alt="" />    </a>
                     <a href="" id={styles.nav_youtube   }><img src="../imgs/yt.svg"        className="nav_img" alt="" />  </a>
-                    </div>
+                </div>
                 </nav>
                 <hr id={styles.linha} />
                 <p id={styles.texto_footer}>Copyright © 2025 VH Burguer | Todos os direitos reservados</p>
