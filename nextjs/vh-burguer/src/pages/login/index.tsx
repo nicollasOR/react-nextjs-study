fd4ae7b5e971037afd182edb92b9dbd48898ac1f
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-27 16:37:13 -0300

    🔨 feat: updated login with auth 🔨

--- nextjs/vh-burguer/src/pages/login/index.tsx
+++ nextjs/vh-burguer/src/pages/login/index.tsx
@@ -1,6 +1,22 @@
 import styles from "./login.module.css"
+import { useState } from "react";
+// import { login } from "../api/AuthService";
+
 
 const Login = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [senha, setSenha] = useState<string>("");
+    function autenticar(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        try{
+            login(email, senha);
+            console.log("tentei")
+        }catch(e: any){
+            alert(e.message);
+        }
+    } 
+
     return (
 
         <>
@@ -11,11 +27,15 @@ const Login = () => {
                     <form className={styles.formulario}>
                         <div className={styles.campo_form}>
                             <label htmlFor="email">E-mail</label>
-                            <input type="text" name="email" placeholder="email@exemplo.com" required />
+                            <input type="text" name="email" placeholder="email@exemplo.com" required 
+                                value={email} onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className={styles.campo_form}>
                             <label htmlFor="senha">Senha</label>
-                            <input type="password" name="senha" placeholder="*******" required />
+                            <input type="password" name="senha" placeholder="*******" required
+                                value={senha} onChange={(e) => setSenha(e.target.value)}
+                            />
                         </div>
                         <a className={styles.esq_senha} href="">Esqueceu sua senha?</a>
                         <button>Entrar</button>
