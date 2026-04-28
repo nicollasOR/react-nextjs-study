e514a97dac86d283c9e8370f2392fc4b9243f334
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-28 11:01:43 -0300

    🔨 feat: add post and authorize on login 🔨

--- nextjs/vh-burguer/src/pages/login/index.tsx
+++ nextjs/vh-burguer/src/pages/login/index.tsx
@@ -1,41 +1,53 @@
 import styles from "./login.module.css"
 import { useState } from "react";
-// import { login } from "../api/AuthService";
+import { login } from "../api/authService";
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast} from "react-toastify";
 
 
 const Login = () => {
-
     const [email, setEmail] = useState<string>("");
     const [senha, setSenha] = useState<string>("");
-    function autenticar(e: React.FormEvent<HTMLFormElement>){
+
+    const routes = useRouter();
+
+    const notificacao = (msg : string) => toast(msg);
+    async function autenticar(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault();
-        try{
-            login(email, senha);
-            console.log("tentei")
-        }catch(e: any){
+        try
+        {
+            await login(email, senha);
+            notificacao("Login bem-sucedido");
+            setTimeout(() =>{
+                routes.push("home/"); 
+
+            }, 2000)
+        }
+        catch(e: any){
             alert(e.message);
         }
     } 
-
+    
     return (
-
+        
         <>
+            <ToastContainer/>
             <main className={styles.main}>
                 <img src="../imgs/hamburguer_login.png" alt="Hambúrguer com ingredientes flutuando em camadas sobre fundo escuro."/>
                 <div className={styles.campo_login}>
                     <h1>Login</h1>
-                    <form className={styles.formulario}>
+                    <form className={styles.formulario} onSubmit={autenticar}>
                         <div className={styles.campo_form}>
                             <label htmlFor="email">E-mail</label>
                             <input type="text" name="email" placeholder="email@exemplo.com" required 
                                 value={email} onChange={(e) => setEmail(e.target.value)}
-                            />
+                                />
                         </div>
                         <div className={styles.campo_form}>
                             <label htmlFor="senha">Senha</label>
                             <input type="password" name="senha" placeholder="*******" required
                                 value={senha} onChange={(e) => setSenha(e.target.value)}
-                            />
+                                />
                         </div>
                         <a className={styles.esq_senha} href="">Esqueceu sua senha?</a>
                         <button>Entrar</button>
