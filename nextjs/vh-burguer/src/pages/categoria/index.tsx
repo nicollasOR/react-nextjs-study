bc6eaad0709ca581e82fd517360ab5c3d5e77b4b
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-28 11:00:46 -0300

    🔨 feat: add post 🔨

--- nextjs/vh-burguer/src/pages/categoria/index.tsx
+++ nextjs/vh-burguer/src/pages/categoria/index.tsx
@@ -1,16 +1,28 @@
 import Footer from '@/components/footer/footer';
 import style from './categoria.module.css'
 import Sub_header from '@/components/sub-header/sub_header';
+import {cadastrarCategoria} from '../api/categoriaService';
+import { useState } from 'react';
 const criarCategoria = () =>{
+    const [categoria, setCategoria] = useState<string>("");
+    function cadastrar(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        cadastrarCategoria(categoria);
+
+        
+    };
+
     return(
         <>
         <Sub_header/>
         <article id={style.main} className="layout_guide">
             <h1> Criar categoria</h1>
-                <form action="">
+                <form action="" onSubmit={cadastrar}>
                     <div id={style.form}>
                     <label htmlFor="nome">Nome categoria</label>
-                    <input type="text" name='nome' required placeholder='Premium'/>
+                    <input type="text" name='nome' required placeholder='Premium'
+                        value={categoria} onChange={(e) => setCategoria(e.target.value)}
+                    />
                     </div>
                     <div className={style.enviar_botoes}>
                         <button>Cancelar</button>
