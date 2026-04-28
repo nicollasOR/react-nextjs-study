d9fbb6a42d6f42e7e376746428895e97ae3238bc
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-04-28 10:30:16 -0300

    🔨 feat: added tokenSecurity 🔨

--- nextjs/vh-burguer/src/pages/api/authService.ts
+++ nextjs/vh-burguer/src/pages/api/authService.ts
@@ -1,3 +1,4 @@
+import secureLocalStorage from "react-secure-storage";
 import { api } from "./api";
 export async function login(email: string, senha: string){
     try{
@@ -5,7 +6,14 @@ export async function login(email: string, senha: string){
         const response = await api.post("Autenticacao/login", {email, senha});
 
         console.log("eba deu certo");
-        console.log(response.data.token);
+        // console.log(response.data.token);
+
+        const token = response.data.token;
+
+
+        secureLocalStorage.setItem("tokenUser", token);
+
+
 
     }catch(error: any){
         throw new Error("Email ou senha inválidos");
