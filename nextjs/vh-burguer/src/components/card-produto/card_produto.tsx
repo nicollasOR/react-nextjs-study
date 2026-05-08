7a87df68cea85394c35a86e75143a44d04cffc82
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-05-08 11:01:04 -0300

    � feat: updating  �

--- nextjs/vh-burguer/src/components/card-produto/card_produto.tsx
+++ nextjs/vh-burguer/src/components/card-produto/card_produto.tsx
@@ -1,44 +1,77 @@
-import style from './cardProduto.module.css'
+import style from "./cardProduto.module.css";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleInfo, faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { formatarPreco } from '@/utils/formatacao'
-import Link from 'next/link'
-import { verificarAutenticacao } from '@/utils/autenticacao'
-import { useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleInfo,
+  faTrashCan,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { formatarPreco } from "@/utils/formatacao";
+import Link from "next/link";
+import { verificarAutenticacao } from "@/utils/autenticacao";
+import { useState } from "react";
 
 type Produto = {
-    titulo: string,
-    descricao: string,
-    img: string,
-    preco: number,
-    produtoId : number,
-    onDelete: (produtoId: number) => void,
-    usuarioAutenticado: boolean
-}
+  titulo: string;
+  descricao: string;
+  img: string;
+  preco: number;
+  produtoId: number;
+  onDelete: (produtoId: number) => void;
+  usuarioAutenticado: boolean;
+};
 
-
-const CardProduto = ({titulo, descricao, img, preco, produtoId, onDelete, usuarioAutenticado} : Produto) => {
-
-    return(
-                <li className={style.lista_cardapios} >
-                    <Link href={"/detalhe_produto/" + produtoId}>
-                    <img src={img} className={style.ex_card} alt="Link da imagem representada pelas imagens dos produtos" />
-                    </Link>
-                    <h4>{titulo}</h4>
-                    <p>{descricao}</p>
-                        <div className={style.botoes}>
-                        <span>{formatarPreco(preco)}</span>
-                        {usuarioAutenticado ?? (
-                        <div className={style.botoes_adm}>
-                        <Link href={"/historico/" + produtoId}><button><FontAwesomeIcon icon={faCircleInfo} className={style.icone_botao} /></button></Link>
-                        <Link href={"/produto?id=" + produtoId}><button><FontAwesomeIcon icon={faPenToSquare} className={style.icone_botao}/></button></Link>
-                        <button onClick={() => onDelete(produtoId)}><FontAwesomeIcon icon={faTrashCan}    className={style.icone_botao}/></button>
-                        </div>
-                        )}                        
-                    </div>
-                </li>
-    )
-}
+const CardProduto = ({
+  titulo,
+  descricao,
+  img,
+  preco,
+  produtoId,
+  onDelete,
+  usuarioAutenticado,
+}: Produto) => {
+  return (
+    <li className={style.lista_cardapios}>
+      <Link href={"/detalhe_produto/" + produtoId}>
+        <img
+          src={img}
+          className={style.ex_card}
+          alt="Link da imagem representada pelas imagens dos produtos"
+        />
+      </Link>
+      <h4>{titulo}</h4>
+      <p>{descricao}</p>
+      <div className={style.botoes}>
+        <span>{formatarPreco(preco)}</span>
+        {usuarioAutenticado && (
+          <>
+            <Link href={"/historico/" + produtoId}>
+              <button>
+                <FontAwesomeIcon
+                  icon={faCircleInfo}
+                  className={style.icone_botao}
+                />
+              </button>
+            </Link>
+            <Link href={"/produto?id=" + produtoId}>
+              <button>
+                <FontAwesomeIcon
+                  icon={faPenToSquare}
+                  className={style.icone_botao}
+                />
+              </button>
+            </Link>
+            <button onClick={() => onDelete(produtoId)}>
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                className={style.icone_botao}
+              />
+            </button>
+          </>
+        )}
+      </div>
+    </li>
+  );
+};
 
 export default CardProduto;
